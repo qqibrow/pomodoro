fad4dc24f64381dc59b8a89ff12902d9284c949f
qqibrow <qqibrow@gmail.com>
2014-06-16 14:13:21 -0500

    could delete data normally.

--- app/src/main/java/com/pomodoro/activities/MainActivity.java
+++ app/src/main/java/com/pomodoro/activities/MainActivity.java
@@ -11,11 +11,13 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.nhaarman.listviewanimations.itemmanipulation.AnimateDismissAdapter;
 import com.nhaarman.listviewanimations.itemmanipulation.OnDismissCallback;
 import com.pomodoro.adapters.TaskAdapter;
 import com.pomodoro.data.Task;
@@ -28,32 +30,52 @@ public class MainActivity extends ActionBarActivity {
 
     TaskAdapter taskAdapter;
     List<Task> taskDataSource;
+    AnimateDismissAdapter animateDismissAdapter;
+
+    List<Integer> mSelectedPositions = new ArrayList<Integer>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        taskAdapter = new TaskAdapter(this, new ArrayList<Task>());
-
-
 
         final ListView listView = (ListView)findViewById(R.id.listView);
-        listView.setAdapter(taskAdapter);
 
         // Load data from database at first time.
         taskDataSource = Task.getAll();
-        taskAdapter.addAll(taskDataSource);
+        taskAdapter = new TaskAdapter(this, taskDataSource);
+        listView.setAdapter(taskAdapter);
 
+        /*
+         Create animation dismissAdapter using baseAdapter.
+         It is not necessary to use a global reference here but I have to because I need to use
+         the reference in OnselectedMenu function. If I can use this reference to create a function
+         here then I only need to save the function. Or, I can set the listener for the menu item,
+         if there is a way.
+          */
 
-
+//        animateDismissAdapter = new AnimateDismissAdapter(taskAdapter, new MyOnDismissCallback());
+//        animateDismissAdapter.setAbsListView(listView);
+//        listView.setAdapter(animateDismissAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Task task = (Task)parent.getItemAtPosition(position);
-                Toast.makeText(MainActivity.this, task.getName(), Toast.LENGTH_SHORT).show();
+
+//                System.out.println("Position " + position + " is selected");
+//                CheckBox cb = (CheckBox)view.findViewById(R.id.taskSelectCheckBox);
+//                cb.toggle();
+//                if(cb.isChecked()) {
+//                    mSelectedPositions.add(position);
+//                } else {
+//                    mSelectedPositions.remove((Integer)position);
+//                }
+
             }
         });
+
         // Init EditText
         final EditText createNewTaskEditText = (EditText)this.findViewById(R.id.input_task);
         createNewTaskEditText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
@@ -70,7 +92,7 @@ public class MainActivity extends ActionBarActivity {
                     to the tail of the list, which is wrong because the newest
                     should show up in the front. Therefore, I use insert here.
                      */
-                    taskAdapter.insert(t, 0);
+                    taskAdapter.addNewItem(t);
                     t.save();
                     // Do the clear stuffs.
                     // Give up focus to the list view.
@@ -106,6 +128,8 @@ public class MainActivity extends ActionBarActivity {
         public void onDismiss(final AbsListView listView, final int[] reverseSortedPositions) {
             for (int position : reverseSortedPositions) {
                 removeItem(position);
+                String info = String.format("Position %d is about to delete.", position);
+                System.out.println(info);
             }
         }
     }
@@ -131,6 +155,11 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
+        }
+        else if(id == R.id.action_search) {
+            //animateDismissAdapter.animateDismiss(mSelectedPositions);
+            //mSelectedPositions.clear();
+            taskAdapter.removeItem(0);
         }
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/pomodoro/adapters/TaskAdapter.java
+++ app/src/main/java/com/pomodoro/adapters/TaskAdapter.java
@@ -8,26 +8,74 @@ import android.widget.ArrayAdapter;
 import com.pomodoro.activities.R;
 import com.pomodoro.data.Task;
 import android.content.Context;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by lniu on 5/17/14.
  */
 public class TaskAdapter extends ArrayAdapter<Task> {
-    public TaskAdapter(Context context, ArrayList<Task> tasks) {
+
+    List<Boolean> itemChecked; // track which position has been selected.
+
+    final static int First_SLOT = 0;
+
+    public TaskAdapter(Context context, List<Task> tasks) {
         super(context, R.layout.row, tasks);
+
+        /*
+         Init items to label which task is checked or not.
+          */
+        itemChecked = new ArrayList<Boolean>(tasks.size());
+        for(int i = 0; i < tasks.size(); ++i)
+            itemChecked.add(false);
+    }
+
+    public void addNewItem(Task t) {
+        // Insert actual data into adapter.
+        this.insert(t, First_SLOT);
+
+        // Insert data into item checked list.
+        this.itemChecked.add(First_SLOT, false);
+    }
+
+    public void removeItem(int position) {
+        itemChecked.remove(position);
+        Task item2remove = this.getItem(position);
+        item2remove.delete();
+        super.remove(item2remove);
+        notifyDataSetChanged();
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         Task task = getItem(position);
         if(convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.row, parent, false);
         }
         TextView tvName = (TextView)convertView.findViewById(R.id.tvName);
         tvName.setText(task.getName());
+
+        CheckBox cb = (CheckBox)convertView.findViewById(R.id.taskSelectCheckBox);
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                if (isChecked) {
+                    itemChecked.set(position, true);
+                } else {
+                    itemChecked.set(position, false);
+                }
+            }
+        });
+        cb.setChecked(itemChecked.get(position));
+
         return convertView;
     }
 
